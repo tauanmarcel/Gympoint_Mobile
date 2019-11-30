caafeb49b583c9b42bd8515c278276c5e784c7b4
Tauan Marcel <tauanmarcel@hotmail.com>
2019-11-30 16:46:03 -0300

    Commit inicial. Eslint configurado

---
+++ src/index.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import {View, Text} from 'react-native';
+
+// import { Container } from './styles';
+
+import Teste from '~/components/Test';
+
+export default function src() {
+  return (
+    <View>
+      <Text>Initial Page</Text>
+      <Teste />
+    </View>
+  );
+}
